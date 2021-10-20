1747152551ac64730c505d9ff31e5b4638d4bce4
Eloi Stree <eloi.Stree@activeme.be>
2021-10-20 22:47:14 +0200

    Save: 10/20/2021 10:47 PM

---
+++ Runtime/UnityEventStateChangeRGB.cs
@@ -0,0 +1,13 @@
+
+using Eloi;
+
+[System.Serializable]
+public class UnityEventStateChangeRGB : UnityEventStateChangeEnum<S_RGBSTATEMACHINE>
+{
+
+}
+[System.Serializable]
+public class UnityEventStateChangeRGBWithSource : UnityEventStateChangeWithFineStateEnum<S_RGBSTATEMACHINE>
+{
+
+}
